303c14339a6037f3adf7e41adf44be2ac92d7654
Gilmar F <gilmaresende>
2024-09-25 19:37:12 -0300

    Rest template

--- hr-payroll/src/main/java/com/github/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/github/hr_payroll/services/PaymentService.java
@@ -1,12 +1,28 @@
 package com.github.hr_payroll.services;
 
 import com.github.hr_payroll.entities.Payment;
+import com.github.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String hostWorker;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPaymeny(Long idWorker, int days) {
-        return new Payment("Bob", 100D, days);
+        Map<String, String> params = new HashMap<>();
+        params.put("id", idWorker.toString());
+        Worker worker = restTemplate.getForObject(String.format("%s/workers/{id}", hostWorker), Worker.class, params);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
